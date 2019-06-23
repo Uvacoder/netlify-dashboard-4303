09f2b4a7e75d670f42b2cf2575bef0270c878a82
David Leininger <davidbleininger@gmail.com>
2019-06-23 00:41:30 -0500

    initial commit...

---
+++ gridsome.server.js
@@ -0,0 +1,38 @@
+// Server API makes it possible to hook into various parts of Gridsome
+// on server-side and add custom data to the GraphQL data layer.
+// Learn more: https://gridsome.org/docs/server-api
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+require('dotenv').config()
+const NetlifyAPI = require('netlify')
+const netlify = new NetlifyAPI(process.env.NETLIFY_ACCESS_KEY)
+
+
+module.exports = function (api) {
+  api.loadSource(async ({ addContentType }) => {
+    const sites = await netlify.listSites()
+    const contentType = addContentType({
+      typeName: 'Sites'
+    })
+
+    for (const site of sites) {
+      contentType.addNode({
+        id: site.id,
+        fields: {
+          name: site.name,
+          url: site.url,
+          adminUrl: site.admin_url,
+          screenshot: site.screenshot_url,
+          repo: site.build_settings.repo_url,
+          provider: site.build_settings.provider,
+        }
+      })
+    }
+    // Use the Data Store API here: https://gridsome.org/docs/data-store-api
+  })
+
+  api.createPages(({ createPage }) => {
+    // Use the Pages API here: https://gridsome.org/docs/pages-api
+  })
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,62 @@
+<template>
+  <Layout>
+    <h1>Nelify Dashboard</h1>
+
+    <div v-for="site in sites">
+      <header class="flex justify-between">
+        <a :href="site.url"><h3>{{ site.name }}</h3></a>
+        <a :href="site.repo">{{ site.provider }}</a>
+      </header>
+      <a :href="`${site.adminUrl}/deploys`">
+        <img :src="`https://api.netlify.com/api/v1/badges/${site.id}/deploy-status`" />
+      </a>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query Sites {
+  sites: allSites {
+    edges {
+      node {
+        id
+        name
+        url
+        adminUrl
+        repo
+        provider
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  name: "Dashboard",
+  metaInfo: {
+    title: 'Status Report'
+  },
+  computed: {
+    sites () {
+      return this.$page.sites.edges.map(it => {
+        return {
+          id: it.node.id,
+          name: it.node.name,
+          url: it.node.url,
+          adminUrl: it.node.adminUrl,
+          repo: it.node.repo,
+          screenshot: it.node.screenshot,
+          provider: it.node.provider,
+        }
+      }).sort((a, b) => a.hash > b.hash ? 1 : -1)
+    }
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
