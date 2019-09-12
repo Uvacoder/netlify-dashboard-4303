523ea508c389cc686a1baa53434a5c6f6fb57276
David Leininger <davidbleininger@gmail.com>
2019-09-11 21:02:57 -0500

    hotjar

--- src/main.js
+++ src/main.js
@@ -3,7 +3,23 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  head.script.push({
+    innerHTML: `(function(h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function() {
+          ;(h.hj.q = h.hj.q || []).push(arguments)
+        }
+      h._hjSettings = { hjid: 1482236, hjsv: 6 }
+      a = o.getElementsByTagName('head')[0]
+      r = o.createElement('script')
+      r.async = 1
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+      a.appendChild(r)
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')`,
+    body: false,
+  })
 }
