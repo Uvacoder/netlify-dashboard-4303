a2cbbf55c582a91ae8ee815ee74a3e5f1922b528
David Leininger <davidbleininger@gmail.com>
2020-03-11 07:08:27 -0500

    Add Tailwind

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,7 +1,15 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const tailwindcss = require('tailwindcss')
 
 module.exports = {
   siteName: 'Gridsome',
   plugins: [],
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [tailwindcss],
+      },
+    },
+  },
 }
