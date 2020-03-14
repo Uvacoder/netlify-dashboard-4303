94d653e1d5b1a108985fa5f57e071acc020b7365
David Leininger <davidbleininger@gmail.com>
2020-03-14 12:02:40 -0500

    svg sizing

--- src/components/github.vue
+++ src/components/github.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <svg
-      width="1024"
-      height="1024"
+      width="100%"
+      height="100%"
       viewBox="0 0 1024 1024"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
