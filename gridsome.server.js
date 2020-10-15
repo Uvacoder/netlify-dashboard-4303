f9f6e0e292794e6789b5e28ccbea7df2b556f69b
David Leininger <davidleininger@hey.com>
2020-10-14 21:03:05 -0500

    remove unused code

--- gridsome.server.js
+++ gridsome.server.js
@@ -30,8 +30,4 @@ module.exports = function(api) {
     }
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api
   })
-
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api
-  })
 }
